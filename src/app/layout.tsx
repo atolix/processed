b066683669e736feca54c18d67292cf2fad68854
atolix <82761106+atolix@users.noreply.github.com>
2024-08-16 21:20:59 +0900

    move header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "../components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +11,8 @@ export const metadata: Metadata = {
     {
       url: "/favicons/favicon.ico",
       rel: "icon",
-    }
-  ]
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -21,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components/header";
 export default function Home() {
   return (
     <div>
-      <Header />
+      <h1>Home</h1>
     </div>
   );
 }
