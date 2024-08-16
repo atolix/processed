1c0ad6ed54bb2156d7b40995cc36b6209ea84f5a
atolix <82761106+atolix@users.noreply.github.com>
2024-08-16 22:36:55 +0900

    articels card

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -1,7 +1,10 @@
 export default function Post({ params }): { params: { slug: string } } {
   return (
-    <div>
-      <h1>Post {params.slug}</h1>
+    <div className="mx-auto grid w-full max-w-6xl items-start gap-12 md:grid-cols-[1fr_180px] lg:grid-cols-[1fr_250px]">
+      <div className="grid w-full flex-1 gap-6">
+        "loremp ipsum"
+      </div>
+      <h1>Article</h1>
     </div>
   );
 }
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,11 +1,38 @@
+import { client } from "@/lib/client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export const metadata = {
-  title: "Posts",
+  title: "Articles",
 };
 
-export default function Posts() {
+export default async function Articles() {
+  const data = await client.get({ endpoint: "articles" });
+
   return (
-    <div>
-      <h1>Posts</h1>
+    <div className="mx-auto grid w-full max-w-6xl items-start gap-12">
+      <div className="grid w-full flex-1 gap-6 grid-cols-1 md:grid-cols-2">
+        {data.contents.map((article) => (
+          <Card key={article.id}>
+            <CardHeader>
+              <img src={article.eyecatch.url} alt={article.title} />
+            </CardHeader>
+            <CardContent>
+              <CardTitle>{article.title}</CardTitle>
+              <CardDescription>description</CardDescription>
+            </CardContent>
+            <CardFooter>
+              <p>{article.publishedAt}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
