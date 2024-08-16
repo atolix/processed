8cc08224922534fb2a15574cf5e3a45c4dfc7ae0
atolix <82761106+atolix@users.noreply.github.com>
2024-08-16 21:18:26 +0900

    add pages

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Posts",
+};
+
+export default function Posts() {
+  return (
+    <div>
+      <h1>Posts</h1>
+    </div>
+  );
+}
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Profile",
+};
+
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile</h1>
+    </div>
+  );
+}
