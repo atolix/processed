b301a544371b2fa2a6184425e1a2637b9f9fd513
atolix <82761106+atolix@users.noreply.github.com>
2024-08-16 23:36:40 +0900

    markdown

--- src/app/articles/[slug]/page.tsx
+++ src/app/articles/[slug]/page.tsx
@@ -2,20 +2,29 @@ import { client } from "@/lib/client";
 import { formattedPublishedAt } from "@/lib/utils";
 
 export default async function Post({ params }): { params: { slug: string } } {
-  const data = await client.get({ endpoint: "articles", contentId: params.slug });
+  const data = await client.get({
+    endpoint: "articles",
+    contentId: params.slug,
+  });
 
   return (
     <div className="mx-auto grid w-full max-w-6xl items-start gap-12 md:grid-cols-[1fr_180px] lg:grid-cols-[1fr_250px]">
-      <div className="grid w-full flex-1 gap-6">
-        <div>
-          <h2>{data.title}</h2>
-          <p>{formattedPublishedAt(data.publishedAt)}</p>
+      <div className="grid w-full flex-1 gap-6 p-3">
+        <div className="flex flex-col space-y-1.5">
+          <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
+            {data.title}
+          </h1>
+          <p className="text-sm text-muted-foreground mt-2">
+            {formattedPublishedAt(data.publishedAt)}
+          </p>
         </div>
-        <div>
-          <p>{data.content}</p>
-        </div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `${data.content}`,
+          }}
+        ></div>
       </div>
-      <h1>Article</h1>
+      <div className="flex flex-col space-y-1.5 p-3">aaaaaa</div>
     </div>
   );
 }
