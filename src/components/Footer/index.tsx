ca7ea7b0df0965cb34366edb6da89d0ac3bf45e6
atolix <82761106+atolix@users.noreply.github.com>
2024-08-17 00:12:38 +0900

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,30 @@
+import { Separator } from "../ui/separator"
+
+export const Footer = () => {
+  return (
+    <footer className="container text-center h-14 max-w-screen-2xl items-center justify-center mt-12">
+      <Separator />
+      <nav className="flex justify-center items-center gap-4 text-sm lg:gap-6 mt-3">
+        <a
+          href="https://x.com/atolix_"
+          className="transition-colors hover:text-foreground/80 text-foreground/60"
+        >
+          Twitter
+        </a>
+        <a
+          href="https://github.com/atolix"
+          className="transition-colors hover:text-foreground/80 text-foreground/60"
+        >
+          Github
+        </a>
+        <a
+          href="/https://open.spotify.com/user/j99idhbgg0ogba7hgqri5ntsg"
+          className="transition-colors hover:text-foreground/80 text-foreground/60"
+        >
+          Spotify
+        </a>
+      </nav>
+      <p className="text-sm text-muted-foreground p-3">© 2024 *processed_ by atolix</p>
+    </footer>
+  );
+}
