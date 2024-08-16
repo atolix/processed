d26e42a0e79c17be3193c5c7b74499bd1888d5d5
atolix <82761106+atolix@users.noreply.github.com>
2024-08-17 00:45:32 +0900

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="ja">
       <body className={inter.className}>
         <Header />
-        <div className="flex flex-col min-h-screen">{children}</div>
+        <div className="flex flex-col min-h-screen container mt-6">{children}</div>
         <Footer />
       </body>
     </html>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,8 +4,11 @@ export const metadata = {
 
 export default function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div className="container text-center max-w-screen-2xl items-center justify-center">
+      <h2 className="font-semibold pb-3">atolix</h2>
+      <h3 className="text-sm p-1">job: Web Backend Engineer(for 3 years)</h3>
+      <h3 className="text-sm p-1">tech stack: Ruby, Typescript, AWS</h3>
+      <h3 className="text-sm p-1">interest: Synthesizer, Electric bass, Fashion</h3>
     </div>
   );
 }
