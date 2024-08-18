34f1718eb184688b9c8b7e83192535baec532196
atolix <82761106+atolix@users.noreply.github.com>
2024-08-18 23:21:34 +0900

    add icon

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaTwitter } from "react-icons/fa"
+
 export const metadata = {
   title: "Profile",
 };
@@ -6,6 +8,22 @@ export default function Profile() {
   return (
     <div className="container text-center max-w-screen-2xl items-center justify-center">
       <h2 className="font-semibold pb-3">atolix</h2>
+      <nav className="flex justify-center items-center gap-4 text-sm lg:gap-6 mb-3">
+        <a
+          href="https://x.com/atolix_"
+          target="_blank"
+          className="transition-colors hover:text-foreground/80 text-foreground/60"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://github.com/atolix"
+          target="_blank"
+          className="transition-colors hover:text-foreground/80 text-foreground/60"
+        >
+          <FaGithub />
+        </a>
+      </nav>
       <h3 className="text-sm p-1">location: Tokyo</h3>
       <h3 className="text-sm p-1">job: Web Backend Developer(for 2 years)</h3>
       <h3 className="text-sm p-1">tech stack: Ruby, Typescript, AWS</h3>
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,28 +4,8 @@ export const Footer = () => {
   return (
     <footer className="container text-center h-14 max-w-screen-2xl items-center justify-center mt-12">
       <Separator />
-      <nav className="flex justify-center items-center gap-4 text-sm lg:gap-6 mt-3">
-        <a
-          href="https://x.com/atolix_"
-          className="transition-colors hover:text-foreground/80 text-foreground/60"
-        >
-          Twitter
-        </a>
-        <a
-          href="https://github.com/atolix"
-          className="transition-colors hover:text-foreground/80 text-foreground/60"
-        >
-          Github
-        </a>
-        <a
-          href="https://open.spotify.com/user/j99idhbgg0ogba7hgqri5ntsg"
-          className="transition-colors hover:text-foreground/80 text-foreground/60"
-        >
-          Spotify
-        </a>
-      </nav>
-      <p className="text-sm text-muted-foreground pt-3">This site uses Google Analytics.</p>
-      <p className="text-sm text-muted-foreground p-3">© 2024 atolix</p>
+      <p className="text-sm text-muted-foreground pt-4">This site uses Google Analytics.</p>
+      <p className="text-sm text-muted-foreground p-3">©2024 atolix</p>
     </footer>
   );
 }
